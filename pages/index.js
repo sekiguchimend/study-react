09c77a4aff72665f9f20f15f8751b2d101814c01
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-03-05 15:56:16 +0900

    🐛

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import {Comport} from "../comport/Comport";
+import { Headline } from "@/comport/Headline";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -15,29 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <Headline title="index page"/>
 
         <div className={styles.center}>
           <Image
